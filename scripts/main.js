4eb5acdccc05f1bba45100d1d6c0e048ef15ef74
nanosuke <82511590+nanosuke@users.noreply.github.com>
2022-06-15 16:50:47 +0900

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "../images/cat.jpg") {
+    myImage.setAttribute("src", "../images/rabbit.png");
+  } else {
+    myImage.setAttribute("src", "../images/cat.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  let myName = prompt("名前を入力してください。");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.innerHTML = "Mozilla is cool, " + myName;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.innerHTML = "Mozilla is cool, " + storedName;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
